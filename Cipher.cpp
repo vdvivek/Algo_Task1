02b085e1bc38575d116cf9364cae3b723dbce515
Vivek Denny <vdvivekdenny@gmail.com>
2018-05-22 01:37:31 +0530

    Cipher complete

--- Cipher.cpp
+++ Cipher.cpp
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void encrypt(int k, char s[0], int p)
+void encrypt(int k, char s[0], int p, int n)
 {
 	int i;
-	while(k > 26)
-	k = k - 26;
 	for(i = 0; i <= p; i ++)
 	{
 		if((s[i] >= 'A') && (s[i] <= 'Z'))
@@ -23,7 +21,28 @@ void encrypt(int k, char s[0], int p)
 		s[i] = s[i] - k;
 		s[i] = s[i] + k;
 	}
-	printf("ciphertext# %s",s);
+	printf("ciphertext#%d %s\n",n,s);
+}
+void decrypt(int k, char s[0], int p, int n)
+{
+	int i;
+	for(i = 0; i <= p; i ++)
+	{
+		if((s[i] >= 'A') && (s[i] < 'Z'))
+		{
+			if(s[i] - k < 'A')
+			s[i] = s[i] + 26;
+		}
+		else if((s[i] >= 'a') && (s[i] <= 'z'))
+		{
+			if(s[i] - k < 'a')
+			s[i] = s[i] + 26;
+		}
+		else 
+		s[i] = s[i] + k;
+		s[i] = s[i] - k;
+	}
+	printf("plaintext#%d %s\n",n,s);
 }
 
 main()
@@ -32,8 +51,16 @@ main()
 	char c[p];
 	printf("Enter key and no. of messages\n");
 	scanf("%d	%d",&k,&n);
-	printf("Enter the message\n");
-	scanf("%s", &c);
-	p = strlen(c);
-	encrypt(k, &c[0],p);
+	while(k > 26)
+		k = k - 26;
+	for(i = 1; i <= 2*n; i ++)
+	{
+		printf("\nEnter the message\n");
+		scanf("%s", &c);
+		p = strlen(c);
+		if (i <= n)
+		encrypt(k, &c[0], p, i);
+		if (i > n)
+		decrypt(k, &c[0], p, i-n);
+	}
 }
